59c337097258e9c714857439915fdd504bd63741
undrgroundz18-UGz <undrgroundz18@gmail.com>
2025-12-29 15:10:51 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,12 +1,38 @@
-self.addEventListener("install", e=>{
-  e.waitUntil(
-    caches.open("boss-respawn-cache")
-      .then(c=>c.addAll(["./","./index.html"]))
+const CACHE_NAME = "boss-respawn-cache-v1";
+
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
+];
+
+// Install – cache files
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
   );
+  self.skipWaiting();
 });
 
-self.addEventListener("fetch", e=>{
-  e.respondWith(
-    caches.match(e.request).then(r=>r || fetch(e.request))
+// Activate – clean old cache
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+// Fetch – serve cached if offline
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(res => {
+      return res || fetch(event.request);
+    })
   );
 });
